ce2f9968f441961c32bd50bbb96fcaebbdc88cf9
Zazheska Lorenzo <zlorenzo@mcm.edu.ph>
2022-08-09 06:40:36 +0800

    committed

---
+++ Week1_JavaBasic1/GradeAverage.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class GradeAverage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("English\t: ");
+        double english = input.nextInt();
+        System.out.print("Math\t: ");
+        double math = input.nextInt();
+        System.out.print("Science\t: ");
+        double science = input.nextInt();
+        input.close();
+
+        double average = ((english + math + science) / 3);
+        System.out.println("Average\t: " + average);
+    }
+}
