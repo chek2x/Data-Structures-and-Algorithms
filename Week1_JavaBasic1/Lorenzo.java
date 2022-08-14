2efd5e0503689fbbbab78c3e37181370db2feca2
chek2x <zlorenzo@mcm.edu.ph>
2022-08-14 23:36:41 +0800

    Modified LORENZO.java

--- Week1_JavaBasic1/Lorenzo.java
+++ Week1_JavaBasic1/Lorenzo.java
@@ -4,7 +4,7 @@
  * @param A221 - Data Structures and Algorithms (Paired)
  */
 
-public class Lorenzo {
+public class LORENZO {
     public static void main(String[] args) {
         System.out.println("LL            OOOOOOOO     RRRRRRRRRRR");
         System.out.println("LL          OOO       OO   RR        RR");
