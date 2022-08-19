d5f4ba2d5d3a9a1f4952c6c4f1a7a1cbd6aca16b
chek2x <zlorenzo@mcm.edu.ph>
2022-08-19 20:56:34 +0800

    Modified Palindrome_Lorenzo.java

--- Week1_StringAndData/Palindrome_Lorenzo.java
+++ Week1_StringAndData/Palindrome_Lorenzo.java
@@ -12,10 +12,15 @@ public class Palindrome_Lorenzo {
         Ownership.Introduction();
 
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter String\t: ");
-        String word = input.nextLine();
+        String word;
+
+        do {
+            System.out.print("Enter String\t: ");
+            word = input.nextLine();
+            input.close();
+        } while (invalidCheck(word));
+
         input.close();
-        invalidCheck(word);
         System.out.println("Palindrome\t: " + isPalindrome(word) + "\n");
     }
 
@@ -30,22 +35,24 @@ public class Palindrome_Lorenzo {
         return true;
     } // End of isPalindrome()
 
-    public static void invalidCheck(String word) {
+    public static boolean invalidCheck(String word) {
         char[] ch = word.toCharArray();
         char[] specialChar = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
 
         if (word.contains(" ") || word.length() <= 0) {
             System.out.println("\nError. Invalid input. Input only one word.\n");
-            System.exit(0);
+            return true;
         }
 
         for (char x : ch) {
             for (char y : specialChar) {
                 if (x == y) {
                     System.out.println("\nError. Invalid input. Remove special character or uppercase letter.\n");
-                    System.exit(0);
+                    return true;
                 }
             }
         }
+
+        return false;
     } // End of invalidCheck()
 }
