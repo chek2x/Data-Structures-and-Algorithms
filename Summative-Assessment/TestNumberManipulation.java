77507c93f3d7eaa141ad13f3d2a4379ed16c8b45
chek2x <zlorenzo@mcm.edu.ph>
2022-08-22 10:26:16 +0800

    Modified TestNumberManipulation.java

--- Summative-Assessment/TestNumberManipulation.java
+++ Summative-Assessment/TestNumberManipulation.java
@@ -1,8 +1,59 @@
+
+/**
+ * @author Lorenzo, Zazheska D.
+ * @param CCIS - CS 2nd Year
+ * @param A221 - Data Structures and Algorithms (Paired)
+ */
+
+import java.util.Scanner;
+
 public class TestNumberManipulation {
     public static void main(String[] args) {
-        double testArr[] = { 5, 3, 2, 6, 7 };
-        double maxEven = NumberManipulation.minOdd(testArr);
+        Ownership.Introduction();
 
-        System.out.println(maxEven);
+        Scanner input = new Scanner(System.in);
+        double arr[];
+        int i;
+
+        while (true) {
+            i = 0;
+
+            System.out.print("Enter array length: ");
+            int len = input.nextInt();
+
+            if (len < 5 || len > 100) {
+                System.out.println("Invalid length. Try again.\n");
+                continue;
+            }
+
+            arr = new double[len];
+
+            while (i < len) {
+                System.out.print("Enter element " + (i + 1) + ": ");
+                arr[i] = input.nextDouble();
+
+                if (arr[i] < 0) {
+                    System.out.println("Invalid element. Try again.\n");
+                    continue;
+                }
+
+                i++;
+            }
+
+            System.out.print("\nYour array sorted: ");
+            NumberManipulation.sort(arr);
+            for (i = 0; i < len; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println("\nHighest Value\t\t: " + NumberManipulation.max(arr));
+            System.out.println("Highest Even Value\t: " + NumberManipulation.maxEven(arr));
+            System.out.println("Highest Odd Value\t: " + NumberManipulation.maxOdd(arr));
+            System.out.println("Lowest Value\t\t: " + NumberManipulation.min(arr));
+            System.out.println("Lowest Even Value\t: " + NumberManipulation.minEven(arr));
+            System.out.println("Lowest Odd Value\t: " + NumberManipulation.minOdd(arr));
+
+            input.close();
+            break;
+        }
     }
 }
