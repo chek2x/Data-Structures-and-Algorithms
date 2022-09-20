d848f054f265719081bf90e7113043eccecf6847
chek2x <zlorenzo@mcm.edu.ph>
2022-09-21 05:54:14 +0800

    Added multiple files

---
+++ M2_Hands-on_Exam/LNodeMenu.java
@@ -0,0 +1,357 @@
+
+/**
+ * @author Lorenzo, Zazheska D.
+ * @param CCIS - CS 2nd Year
+ * @param A221 - Data Structures and Algorithms (Paired)
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LNodeMenu {
+    public static void main(String[] args) {
+        Ownership.Introduction();
+
+        String menu[] = { "Add At First", "Add At Position", "Add At Last", "Delete At First", "Delete At Position",
+                "Delete Value",
+                "Delete At Last", "Get First", "Get At Position", "Get Last", "Search", "Edit", "Exit" };
+        String option = "", all = "";
+        int inputVal, inputPos;
+        LNode myNode = new LNode();
+        JPanel valueOnly = new JPanel();
+        JPanel positionOnly = new JPanel();
+        GridBagConstraints a = new GridBagConstraints();
+        GridBagConstraints b = new GridBagConstraints();
+
+        a.gridx = 0;
+        a.gridy = 0;
+        a.weightx = 0;
+        b.fill = GridBagConstraints.HORIZONTAL;
+        b.gridx = 1;
+        b.gridy = 0;
+        b.gridwidth = 2;
+        b.weightx = 0.5;
+
+        JLabel valtxt = new JLabel("Value: ");
+        JLabel postxt = new JLabel("Position: ");
+        JTextField value = new JTextField();
+        JTextField position = new JTextField();
+
+        valueOnly.setLayout(new GridBagLayout());
+        valueOnly.add(valtxt, a);
+        valueOnly.add(value, b);
+
+        positionOnly.setLayout(new GridBagLayout());
+        positionOnly.add(postxt, a);
+        positionOnly.add(position, b);
+
+        try {
+            do {
+                all = "\nEmpty\t: " + myNode.isEmpty() + "\tCapacity\t: " + myNode.currentSize() + "\nElements\t: "
+                        + myNode.traverse();
+
+                option = JOptionPane
+                        .showInputDialog(null, new JTextArea(all + "\nSelect"), "Menu", 1, null, menu, menu[0])
+                        .toString();
+                switch (option) {
+                    case "Add At First":
+                        try {
+                            JOptionPane.showConfirmDialog(null, valueOnly, option, JOptionPane.OK_CANCEL_OPTION);
+
+                            inputVal = Integer.parseInt(value.getText());
+                            if (value.getText() == null || value.getText() == "") {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (myNode.contains(inputVal)) {
+                                JOptionPane.showMessageDialog(null, "Element already exists.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (inputVal <= 0) {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            }
+
+                            myNode.addAtFirst(inputVal);
+                            JOptionPane.showMessageDialog(null, "Element added to first.", option,
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        } catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Invalid input.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                        value.setText("");
+                        position.setText("");
+                        break;
+
+                    case "Add At Position":
+                        try {
+                            if (myNode.isEmpty()) {
+                                JOptionPane.showMessageDialog(null,
+                                        "List is empty. Value will be added to the first position.", option,
+                                        JOptionPane.INFORMATION_MESSAGE);
+                            }
+
+                            JOptionPane.showConfirmDialog(null, valueOnly, option, JOptionPane.OK_CANCEL_OPTION);
+
+                            inputVal = Integer.parseInt(value.getText());
+
+                            if (value.getText() == null || value.getText() == "") {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (myNode.contains(inputVal)) {
+                                JOptionPane.showMessageDialog(null, "Element already exists.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (inputVal <= 0) {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            }
+
+                            JOptionPane.showConfirmDialog(null, positionOnly, option, JOptionPane.OK_CANCEL_OPTION);
+
+                            inputPos = Integer.parseInt(position.getText());
+
+                            if (position.getText() == null || position.getText() == "") {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (Integer.parseInt(position.getText()) > myNode.currentSize()) {
+                                JOptionPane.showMessageDialog(null,
+                                        "Position entered is more than current size. Value will be added to the last position.",
+                                        option, JOptionPane.INFORMATION_MESSAGE);
+                            }
+
+                            myNode.addAtPosition(inputVal, inputPos);
+                        } catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Invalid input.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                        value.setText("");
+                        position.setText("");
+                        break;
+
+                    case "Add At Last":
+                        try {
+                            JOptionPane.showConfirmDialog(null, valueOnly, option, JOptionPane.OK_CANCEL_OPTION);
+
+                            inputVal = Integer.parseInt(value.getText());
+
+                            if (value.getText() == null || value.getText() == "") {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (myNode.contains(inputVal)) {
+                                JOptionPane.showMessageDialog(null, "Element already exists.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (inputVal <= 0) {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            }
+
+                            myNode.addAtLast(inputVal);
+                        } catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Invalid input.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                        value.setText("");
+                        position.setText("");
+                        break;
+
+                    case "Delete At First":
+                        if (myNode.isEmpty()) {
+                            JOptionPane.showMessageDialog(null, "List is empty.", "Error", JOptionPane.ERROR_MESSAGE);
+                        } else {
+                            myNode.deleteAtFirst();
+                            JOptionPane.showMessageDialog(null, "First element was removed.", option,
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        }
+                        break;
+
+                    case "Delete At Position":
+                        try {
+                            if (myNode.isEmpty()) {
+                                JOptionPane.showMessageDialog(null,
+                                        "List is empty.", option,
+                                        JOptionPane.INFORMATION_MESSAGE);
+                                break;
+                            }
+
+                            JOptionPane.showConfirmDialog(null, positionOnly, option, JOptionPane.OK_CANCEL_OPTION);
+
+                            inputPos = Integer.parseInt(position.getText());
+
+                            if (position.getText() == null || position.getText() == "") {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (Integer.parseInt(position.getText()) > myNode.currentSize()) {
+                                JOptionPane.showMessageDialog(null,
+                                        "Invalid position.",
+                                        option, JOptionPane.INFORMATION_MESSAGE);
+                                break;
+                            }
+
+                            myNode.deleteAtPosition(inputPos);
+                        } catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Invalid input.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                        position.setText("");
+                        break;
+
+                    case "Delete At Last":
+                        if (myNode.isEmpty()) {
+                            JOptionPane.showMessageDialog(null, "List is empty.", "Error", JOptionPane.ERROR_MESSAGE);
+                        } else {
+                            myNode.deleteAtLast();
+                            JOptionPane.showMessageDialog(null, "Last element was removed.", option,
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        }
+                        break;
+
+                    case "Get First":
+                        JOptionPane.showMessageDialog(null, myNode.getFirst(), option, JOptionPane.INFORMATION_MESSAGE);
+                        break;
+
+                    case "Get At Position":
+                        try {
+                            JOptionPane.showConfirmDialog(null, positionOnly, option, JOptionPane.OK_CANCEL_OPTION);
+
+                            inputPos = Integer.parseInt(position.getText());
+
+                            if (position.getText() == null || position.getText() == "") {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (Integer.parseInt(position.getText()) > myNode.currentSize()) {
+                                JOptionPane.showMessageDialog(null,
+                                        "Invalid position.",
+                                        option, JOptionPane.INFORMATION_MESSAGE);
+                                break;
+                            }
+
+                            JOptionPane.showMessageDialog(null, myNode.getAtPosition(inputPos), option,
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        } catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Invalid input.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                        position.setText("");
+                        break;
+
+                    case "Get Last":
+                        JOptionPane.showMessageDialog(null, myNode.getLast(), option, JOptionPane.INFORMATION_MESSAGE);
+                        break;
+
+                    case "Search":
+                        try {
+                            JOptionPane.showConfirmDialog(null, valueOnly, option, JOptionPane.INFORMATION_MESSAGE);
+
+                            inputVal = Integer.parseInt(value.getText());
+
+                            if (value.getText() == null || value.getText() == "") {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (inputVal <= 0) {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            }
+
+                            JOptionPane.showMessageDialog(null, myNode.where(inputVal), option,
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        } catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Invalid input.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                        value.setText("");
+                        break;
+
+                    case "Edit":
+                        try {
+                            if (myNode.isEmpty()) {
+                                JOptionPane.showMessageDialog(null,
+                                        "List is empty. Value will be added to the first position.", option,
+                                        JOptionPane.INFORMATION_MESSAGE);
+                            }
+
+                            JOptionPane.showConfirmDialog(null, positionOnly, option, JOptionPane.OK_CANCEL_OPTION);
+
+                            inputPos = Integer.parseInt(position.getText());
+
+                            if (position.getText() == null || position.getText() == "") {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (Integer.parseInt(position.getText()) > myNode.currentSize()) {
+                                JOptionPane.showMessageDialog(null,
+                                        "Invalid position.",
+                                        option, JOptionPane.INFORMATION_MESSAGE);
+                                break;
+                            }
+
+                            JOptionPane.showConfirmDialog(null, valueOnly, option, JOptionPane.OK_CANCEL_OPTION);
+
+                            inputVal = Integer.parseInt(value.getText());
+
+                            if (value.getText() == null || value.getText() == "") {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (myNode.contains(inputVal)) {
+                                JOptionPane.showMessageDialog(null, "Element already exists.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (inputVal <= 0) {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            }
+
+                            myNode.deleteAtPosition(inputPos);
+                            myNode.addAtPosition(inputVal, inputPos);
+                        } catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Invalid input.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                        value.setText("");
+                        position.setText("");
+                        break;
+
+                    case "Delete Value":
+                        try {
+                            if (myNode.isEmpty()) {
+                                JOptionPane.showMessageDialog(null,
+                                        "List is empty.", option,
+                                        JOptionPane.INFORMATION_MESSAGE);
+                                break;
+                            }
+
+                            JOptionPane.showConfirmDialog(null, valueOnly, option, JOptionPane.OK_CANCEL_OPTION);
+
+                            inputVal = Integer.parseInt(value.getText());
+
+                            if (value.getText() == null || value.getText() == "") {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            } else if (inputVal <= 0) {
+                                JOptionPane.showMessageDialog(null, "Invalid input.", "Error",
+                                        JOptionPane.ERROR_MESSAGE);
+                                break;
+                            }
+
+                            JOptionPane.showMessageDialog(null, myNode.deleteValue(inputVal), option,
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        } catch (NumberFormatException e) {
+                            JOptionPane.showMessageDialog(null, "Invalid input.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                        value.setText("");
+                        break;
+
+                }
+            } while (!option.equals("Exit"));
+        } catch (NullPointerException e) {
+            System.exit(0);
+        }
+    }
+}
---
+++ Week7_LinkedList/LNode.java
@@ -0,0 +1,259 @@
+/**
+ * @author Lorenzo, Zazheska D.
+ * @param CCIS - CS 2nd Year
+ * @param A221 - Data Structures and Algorithms (Paired)
+ */
+
+public class LNode {
+    private int data;
+    private LNode head, next;
+    String hold = "";
+
+    public LNode() {
+        head = next = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addAtFirst(int data) {
+        LNode newNode = new LNode();
+        if (isEmpty()) {
+            newNode.data = data;
+            newNode.next = null;
+            head = newNode;
+        } else {
+            newNode.data = data;
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public String traverse() {
+        String hold = " ";
+        LNode link = head;
+        if (isEmpty()) {
+            hold = "List is Empty.";
+        } else {
+            while (link != null) {
+                hold += link.data + "->";
+                link = link.next;
+            }
+        }
+        return "head-> " + hold + " " + link;
+    }
+
+    public void addAtLast(int data) {
+        if (isEmpty()) {
+            addAtFirst(data);
+        } else {
+            LNode visit = head;
+            while (visit.next != null) {
+                visit = visit.next;
+            }
+            LNode newNode = new LNode();
+            newNode.data = data;
+            newNode.next = null;
+            visit.next = newNode;
+        }
+    }
+
+    public void addAtPosition(int data, int position) {
+        if (isEmpty()) {
+            addAtFirst(data);
+        } else if (position == 1) {
+            addAtFirst(data);
+        } else if (position > currentSize()) {
+            addAtLast(data);
+        } else if (position <= 0 || position > currentSize()) {
+            System.out.println("Position is NOT valid.");
+        } else {
+            LNode visit, link;
+            visit = link = head;
+            int ctr = 1;
+            while (ctr != position) {
+                visit = visit.next;
+                ctr++;
+            }
+
+            while (link.next != visit) {
+                link = link.next;
+            }
+
+            LNode newNode = new LNode();
+            newNode.data = data;
+            newNode.next = visit;
+            link.next = newNode;
+        }
+    }
+
+    public int currentSize() {
+        int counter = 1;
+        if (isEmpty()) {
+            counter = 0;
+        } else {
+            LNode visit = head;
+            while (visit.next != null) {
+                visit = visit.next;
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public String deleteAtFirst() {
+        if (isEmpty()) {
+            return ("List is empty.");
+        } else {
+            LNode link = head;
+            head = link.next;
+            return ("First node was deleted successfully.");
+        }
+    }
+
+    public String deleteAtLast() {
+        if (isEmpty()) {
+            return ("List is empty.");
+        } else {
+            LNode link, visit;
+            link = visit = head;
+
+            while (visit.next != null) {
+                visit = visit.next;
+            }
+
+            while (link.next != visit) {
+                link = link.next;
+            }
+
+            link.next = null;
+            return ("Last node was deleted successfully.");
+        }
+    }
+
+    public void deleteAtPosition(int position) {
+        if (isEmpty()) {
+            System.out.println("List is empty.");
+        } else if (position == 1) {
+            deleteAtFirst();
+        } else if (position <= 0 || position > currentSize() + 1) {
+            System.out.println(position + " is NOT valid.");
+        } else {
+            LNode visit, link, pointer;
+            visit = link = pointer = head;
+
+            int ctr = 1;
+
+            while (ctr != position) {
+                visit = visit.next;
+                ctr++;
+            }
+
+            while (link.next != visit) {
+                link = link.next;
+            }
+
+            ctr = 1;
+            while (ctr != position + 1) {
+                pointer = pointer.next;
+                ctr++;
+            }
+
+            link.next = pointer;
+        }
+    }
+
+    public boolean contains(int data) {
+        boolean found = false;
+        if (!isEmpty()) {
+            LNode link = head;
+
+            while (link != null) {
+                if (link.data == data) {
+                    found = true;
+                    break;
+                } else {
+                    link = link.next;
+                }
+            }
+        }
+
+        return found;
+    }
+
+    public String where(int data) {
+        int ctr = 1;
+        if (isEmpty()) {
+            return "List is empty.";
+        } else {
+            LNode link = head;
+
+            while (link != null) {
+                if (link.data == data) {
+                    return data + " is found in position " + ctr + ".";
+                } else {
+                    link = link.next;
+                    ctr++;
+                }
+            }
+
+            return data + " is not found.";
+        }
+    }
+
+    public String getFirst() {
+        if (isEmpty()) {
+            return "List is empty";
+        } else {
+            return "The first element is " + (Object) head.data + ".";
+        }
+    }
+
+    public String getLast() {
+        if (isEmpty()) {
+            return "List is empty";
+        }
+        LNode temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        return "The last element is " + (Object) temp.data + ".";
+    }
+
+    public String getAtPosition(int position) {
+        if (isEmpty()) {
+            return ("List is empty.");
+        } else if (position <= 0 || position > currentSize()) {
+            return ("Invalid position.");
+        } else {
+            LNode temp = head;
+            int ctr = 1;
+            while (ctr != position) {
+                temp = temp.next;
+                ctr++;
+            }
+            return "Position " + position + " contains " + (Object) temp.data + ".";
+        }
+    }
+
+    public String deleteValue(int data) {
+        int pos = 1;
+        LNode link = head;
+
+        if (isEmpty()) {
+            return "List is empty.";
+        } else {
+            while (link != null) {
+                if (link.data == data) {
+                    deleteAtPosition(pos);
+                    return data + " successfully deleted.";
+                } else {
+                    link = link.next;
+                    pos++;
+                }
+            }
+            return data + " is not found.";
+        }
+    }
+}
